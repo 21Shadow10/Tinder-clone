e4a6b3e573762b2dbad214cb462542b3f010c199
Dharmik Govani <63103191+21Shadow10@users.noreply.github.com>
2021-09-26 12:27:00 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header' ;
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
+
+function App() {
+  return (
+    //BEM class naming convention
+    <div className="App">
+
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react' ;
+import './Header.css' ;
+import tinder_logo from './assets/tinder_logo.png' ;
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+
+            <img 
+                className="header__logo"
+                src={tinder_logo}
+                alt=""
+                />
+
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header ;
